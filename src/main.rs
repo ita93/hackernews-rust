5825bcc8664c4526a5832003fe187a8e77f0d68c
Phi Nguyen <phind.uet@gmail.com>
2019-03-03 21:25:54 +0800

    Display rss on a website

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,22 @@
 //In rust 2018, we almostly don't need extern crate anymore.
-use serde;
-use serde_derive;
-use serde_json;
+#![feature(proc_macro_hygiene, decl_macro)]
+use rocket::*;
+use rocket_contrib::templates::Template;
 
 mod fetch;
 use crate::fetch::*;
 
+const RSS_URL: &str = "https://news.ycombinator.com/rss";
+
+#[get("/")]
+fn index() -> Template {
+    let news = fetch_from(RSS_URL).ok().expect("Could not read RSS");
+    Template::render("index", &news)
+}
 
 fn main() {
-
+    rocket::ignite().
+        mount("/", routes![index])
+        .attach(Template::fairing())
+        .launch();
 }
